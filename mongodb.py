5b1c350659c904bc8b4fbff668edd448177daa46
SanjanaV <sanjanav2701@gmail.com>
2021-01-03 20:11:52 +0530

    Day4

--- mongodb.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-import pymongo
-client=MongoClient('mongodb+srv://sanju:<password>@cluster0.wllvz.mongodb.net/<dbname>?retryWrites=true&w=majority')
-db=client.get_database("sentiment_meter")
-records=db.words
